1513f59a70c61bc8bbd19dae0a3ef4ec6d201fbc
Jane Suh <sourcherryyogurt@gmail.com>
2020-01-17 17:24:07 -0500

    Snapshot tested dashboard, tested buttons in control component, tested classes and labels in display component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { render, fireEvent, act } from '@testing-library/react';
+import Dashboard from '../dashboard/Dashboard';
+
 // Test away!
+test('locked toggle button displays unlock after unlocked gate is locked', () => {
+    const { getByText, findByText } = render(<Dashboard locked={false} />);
+    act(() => {
+        fireEvent.click(getByText('Lock Gate'));
+    });
+    findByText(/unlock/i);
+});
+
+test('locked toggle button is disabled if gate is open, closed toggle button is disabled if gate is locked', () => {
+    const { getByText, findByText } = render(<Dashboard locked={false} closed={false} />);
+    expect(getByText(/lock gate/i).disabled).toBe(true);
+    act(() => {
+        fireEvent.click(getByText('Close Gate'));
+    });
+    act(() => {
+        fireEvent.click(getByText('Lock Gate'));
+    });
+    expect(getByText(/open gate/i).disabled).toBe(true);
+});
